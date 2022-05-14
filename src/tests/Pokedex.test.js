a8ba04427617958902ef49193072a28aaf402ba2
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-05-14 00:13:08 -0300

    Requisito 03, feito um forEach para o teste do botão de clicar no próximo

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -13,27 +13,24 @@ describe('5. Teste o componente <Pokedex.js />', () => {
 
   test('5b - exibido o próximo pokémon quando o botão Próximo pokémon é clicado', () => {
     renderWithRouter(<App />);
+    const pokemonsByName = [
+      'Charmander',
+      'Caterpie',
+      'Ekans',
+      'Alakazam',
+      'Mew',
+      'Rapidash',
+      'Snorlax',
+      'Dragonair',
+      'Pikachu',
+    ];
     const pikachu = screen.getByText(/pikachu/i);
     expect(pikachu).toBeInTheDocument();
     const nextPokeButton = screen.getByRole('button', { name: /próximo pokémon/i });
-    userEvent.click(nextPokeButton);
-    expect(pikachu.textContent).toBe('Charmander');
-    userEvent.click(nextPokeButton);
-    expect(pikachu.textContent).toBe('Caterpie');
-    userEvent.click(nextPokeButton);
-    expect(pikachu.textContent).toBe('Ekans');
-    userEvent.click(nextPokeButton);
-    expect(pikachu.textContent).toBe('Alakazam');
-    userEvent.click(nextPokeButton);
-    expect(pikachu.textContent).toBe('Mew');
-    userEvent.click(nextPokeButton);
-    expect(pikachu.textContent).toBe('Rapidash');
-    userEvent.click(nextPokeButton);
-    expect(pikachu.textContent).toBe('Snorlax');
-    userEvent.click(nextPokeButton);
-    expect(pikachu.textContent).toBe('Dragonair');
-    userEvent.click(nextPokeButton);
-    expect(pikachu.textContent).toBe('Pikachu');
+    pokemonsByName.forEach((pokemon) => {
+      userEvent.click(nextPokeButton);
+      expect(pikachu.textContent).toBe(pokemon);
+    });
   });
 
   test('5c - se é mostrado apenas um pokémon por vez', () => {
@@ -54,10 +51,8 @@ describe('5. Teste o componente <Pokedex.js />', () => {
       'Normal',
       'Dragon',
     ];
-
     const countAllButtons = screen.getAllByTestId('pokemon-type-button');
     const condition = 7;
-
     elements.forEach((element) => {
       const buttons = screen.getByRole('button', { name: element });
       expect(buttons).toBeInTheDocument();
